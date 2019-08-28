2c781dd39afda56e20a4f28f1cb255583fa46363
Jakub Petrovič <petrovic.jakub@hotmail.com>
2019-08-28 19:15:09 +0200

    added destination file paths

--- clone-vm.ps1
+++ clone-vm.ps1
@@ -1,10 +1,21 @@
-#Use this script at your own risk!!
+#Use this script at your own risk
+#TODO - dynamically select vmcx files
+
 #Modify the variables to suit your needs
 $vmcxPath = 'D:\Hyper-V exports\CENTOS7-template\Virtual Machines\22C1E1A1-458E-4741-B3AA-D7C46A178F37.vmcx'
-$destinationPath = 'F:\VirtualMachines'
-$newVmName = 
+$newVmName = "IMPORT-TEST"
 
 #Get name for the new VM
+#Write-Output 'Insert vm name:'
+#$newVmName = Read-Host
+
+$destinationPath = 'F:\VirtualMachines\' + $newVmName
 
 #import virtual machine
-Import-VM -Path $vmcxPath -copy  -GenerateNewId
+#TODO - Create folders in final destination for VHDs, snapshots, vms etc 
+Import-VM -Path $vmcxPath `
+    -copy  `
+    -GenerateNewId `
+    -SnapshotFilePath  ($destinationPath + '\Snapshots') `
+    -VhdDestinationPath ($destinationPath + '\Virtual Hard Disks') `
+    -VirtualMachinePath ($destinationPath)
